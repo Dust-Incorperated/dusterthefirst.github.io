3089717998c8824c5f54859c283df5a48fdb887d
DusterTheFirst <DusterTheFirst@gmial.com>
2016-10-02 21:29:38 -0400

    Added Sidebar
    21

--- resources/scripts/moveBody.js
+++ resources/scripts/moveBody.js
@@ -1,15 +1,27 @@
+//Align Body
+//Align Body
 var h = document.getElementById('header').offsetHeight;
 document.getElementById('headerSpacer').style.height = h;
+//Align Menu Button
+var m = document.getElementById('menu').offsetHeight;
+document.getElementById('menu').style.top = h/2 - m/2;
 
+//Timed Align
 var timer = setInterval(move, 10000);
 
+//Aligns All
 function move(){
     var h = document.getElementById('header').offsetHeight;
     document.getElementById('headerSpacer').style.height = h;
+    
+    var m = document.getElementById('menu').offsetHeight;
+    document.getElementById('menu').style.top = h/2 - m/2;
 }
 
+//Unloads Interval
 function unload(){
     clearInterval(timer);
 }
 
+//^^^
 window.onunload = unload;
